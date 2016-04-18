ee4664bcf512b1dae95f72afa47a47352d28ae6d
noelwhite-fmr <noel.white@fmr.com>
2016-04-18 15:13:38 -0400

    update copyright

--- content_lookup.js
+++ content_lookup.js
@@ -1,7 +1,7 @@
 /*
 Jive - Content Lookup
 
-Copyright (c) 2015 Fidelity Investments
+Copyright (c) 2015-2016 Fidelity Investments
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
